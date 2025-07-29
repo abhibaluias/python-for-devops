fd5d711c70a246ac5e73b71fcdccaf2649e304c4
abhibaluias <166683610+abhibaluias@users.noreply.github.com>
2025-07-29 12:16:52 +0530

    push onboard

--- Day-02/examples/00-string-practice.py
+++ Day-02/examples/00-string-practice.py
@@ -1,5 +1,5 @@
 str1 = input("Enter the Name")
 str2 = input("Enter the Surname")p
 result = str1 + " " + str2
-print(f"Hello, {result} ! Welcome onoard")
+print(f"Hello, {result} ! Welcome onboard")
 print(len(result))
